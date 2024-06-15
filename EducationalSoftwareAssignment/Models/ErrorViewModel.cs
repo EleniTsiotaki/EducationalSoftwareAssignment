a7048a894d03a2e986906cd97923245ec38aab79
EleniTsiotaki <73613635+EleniTsiotaki@users.noreply.github.com>
2024-06-15 17:30:15 +0300

    Add project files.

---
+++ EducationalSoftwareAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EducationalSoftwareAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
