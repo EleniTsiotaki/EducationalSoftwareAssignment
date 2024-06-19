6b2fa6f87aa9075cd0f3e67fc123693a2f13be66
EleniTsiotaki <73613635+EleniTsiotaki@users.noreply.github.com>
2024-06-19 10:59:34 +0300

    Added Exercises View

---
+++ EducationalSoftwareAssignment/Controllers/ExercisesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EducationalSoftwareAssignment.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Exercise1()
+        {
+            return View();
+        }
+
+        public IActionResult Exercise2()
+        {
+            return View();
+        }
+
+        public IActionResult Exercise3()
+        {
+            return View();
+        }
+    }
+}
