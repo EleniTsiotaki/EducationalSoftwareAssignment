43475b04649a52bebf27522702984f7d743507f2
Sofiaraf <73124550+Sofiaraf@users.noreply.github.com>
2024-07-09 19:59:06 +0300

    Courses

---
+++ EducationalSoftwareAssignment/Controllers/CoursesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EducationalSoftwareAssignment.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Course11()
+        {
+            return View();
+        }
+
+        public IActionResult Course12()
+        {
+            return View();
+        }
+
+        public IActionResult Course13()
+        {
+            return View();
+        }
+
+        public IActionResult Course21()
+        {
+            return View();
+        }
+
+        public IActionResult Course22()
+        {
+            return View();
+        }
+
+        public IActionResult Course23()
+        {
+            return View();
+        }
+
+        public IActionResult Course31()
+        {
+            return View();
+        }
+
+        public IActionResult Course32()
+        {
+            return View();
+        }
+
+        public IActionResult Course33()
+        {
+            return View();
+        }
+    }
+}
