a720260504d00e098ee67833ebc20ebbad63e0fb
Sofiaraf <73124550+Sofiaraf@users.noreply.github.com>
2024-07-09 14:48:29 +0300

    test1

---
+++ EducationalSoftwareAssignment/Controllers/TestsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EducationalSoftwareAssignment.Controllers
+{
+    public class TestsController : Controller
+    {
+        // GET: Tests/Test1
+        public IActionResult Test11()
+        {
+            return View();
+        }
+    }
+}
